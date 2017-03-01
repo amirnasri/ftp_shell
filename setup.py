d58c6361a85ba8bcca4af38c28c0a1dc1c31b061
Amir <amir@fortinet>
2017-02-28 16:59:10 -0800

    -

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ftpshell',
-    version='1.7',
+    version='1.8',
     author='Amir Nasri',
     author_email='amnasri@gmail.com',
     packages=['ftpshell'],
