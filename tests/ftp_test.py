0e1cc39d10681f8e9083ce363ce7536a9c9a90e4
Amir <amir@fortinet>
2017-05-16 18:50:11 -0700

    -

--- tests/ftp_test.py
+++ tests/ftp_test.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 from ftpshell.ftp.ftp_session import FtpSession
 
 def subprocess_call(cmd):
@@ -15,7 +16,17 @@ def test_ftp_get():
 		#fs.cd(["upload/anasri"])
 		fs.rm([test_file])
 		fs.put(["/tmp/" + test_file])
+		save_stdout = sys.stdout
+		sys.stdout = open("/tmp/stdout", "w")
 		fs.ls([])
+		sys.stdout = save_stdout
+		try:
+			subprocess.check_call(("grep %s /tmp/stdout" % test_file).split())
+		except OSError:
+			assert False
+		os.system("rm /tmp/stdout")
+		subprocess_call("mv /tmp/%s /tmp/%s_copy" % (test_file, test_file))
+		fs.get([test_file])
 	except:
 		raise
 	finally:
