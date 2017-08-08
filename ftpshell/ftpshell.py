564d38d9a21811fdb49b1c3aaf83282d3bea5712
amirnasri <amnasri@gmail.com>
2017-08-07 23:34:01 -0700

    -

--- ftpshell/ftpshell.py
+++ ftpshell/ftpshell.py
@@ -174,42 +174,6 @@ class FtpCli:
             os.waitpid(self.fuse_process_pid, 0)
             print("fuse_process joined!")
 
-'''class Completer(object):
-    """ Class to provide tab-completion functionality
-    to the command line.
-    """
-    def __init__(self, options):
-        self.options = sorted(options)
-        return
-
-    def complete(self, text, state):
-        response = None
-        if state == 0:
-            if text:
-                if text.startswith('put '):
-                    fname_prefix = text[4:]
-                    listdir = os.listdir('.')
-                    self.matches = [s
-                                    for s in listdir
-                                    if s and s.startswith(fname_prefix)]
-                    if len(self.matches) == 1:
-                        self.matches = ["put " + i for i in self.matches]
-
-                else:
-                    self.matches = [s
-                                    for s in self.options
-                                    if s and s.startswith(text)]
-
-            else:
-                self.matches = self.options[:]
-
-        # Return the state'th item from the match list,
-        # if we have that many.
-        try:
-            response = self.matches[state]
-        except IndexError:
-            response = None
-        return response'''
 
 def main():
     # Setup readline to provide tab completion for the command line.
